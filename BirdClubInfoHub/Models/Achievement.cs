eeb41c3e2934542c80fdf3e57e84279169e6fb01
Miraitowa79 <thaouyen7923@gmail.com>
2023-06-26 01:03:05 +0700

    Details Bird + Achievement

---
+++ BirdClubInfoHub/Models/Achievement.cs
@@ -0,0 +1,13 @@
+﻿namespace BirdClubInfoHub.Models
+{
+    public class Achievement
+    {
+        public int Id { get; set; }
+
+        public string TournamentName { get; set; } = string.Empty;
+
+        public string Rank { get; set; } = string.Empty;
+
+        public ICollection<Bird> Bird { get; set; } = new List<Bird>();
+    }
+}
