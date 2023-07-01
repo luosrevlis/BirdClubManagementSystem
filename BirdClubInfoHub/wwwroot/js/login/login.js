c64a6c2c984602f452cb1ce37cf1ff036521b8b4
Vincent Dao <vincentdaomain@gmail.com>
2023-07-02 02:15:35 +0700

    minor update to login layout and blog

--- BirdClubInfoHub/wwwroot/js/login/login.js
+++ BirdClubInfoHub/wwwroot/js/login/login.js
@@ -7,7 +7,7 @@ inputs.forEach((inp) =>{
         inp.classList.add("active")
     });
     inp.addEventListener("blur", ()=>{
-        if(inp.value != "") return
+        if(inp.value !== "") return;
         inp.classList.remove("active");
     });
 });
