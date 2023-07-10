17affe81044a9af8352eaa0d77af8b6f50f3987f
luosrevlis <tuanminhphamtdn@gmail.com>
2023-07-10 11:48:17 +0700

    done request approval

--- BirdClubInfoHub/wwwroot/js/event-registration/index.js
+++ BirdClubInfoHub/wwwroot/js/event-registration/index.js
@@ -1,9 +1,9 @@
 ﻿function initDelete(regId) {
-    document.querySelector(".delete-reg-container").style.display = "flex";
+    document.querySelector(".delete-container").style.display = "flex";
     document.querySelector("#reg-id").value = regId;
 }
 
 function cancelDelete() {
-    document.querySelector(".delete-reg-container").style.display = "none";
+    document.querySelector(".delete-container").style.display = "none";
     document.querySelector("#reg-id").value = "";
 }
--- BirdClubManagementSystem/wwwroot/js/user-management/index.js
+++ BirdClubManagementSystem/wwwroot/js/user-management/index.js
@@ -1,9 +1,9 @@
-﻿function initDelete(regId) {
-    document.querySelector(".delete-reg-container").style.display = "flex";
-    document.querySelector("#reg-id").value = regId;
+﻿function initDelete(userId) {
+    document.querySelector(".delete-container").style.display = "flex";
+    document.querySelector("#user-id").value = userId;
 }
 
 function cancelDelete() {
-    document.querySelector(".delete-reg-container").style.display = "none";
-    document.querySelector("#reg-id").value = "";
+    document.querySelector(".delete-container").style.display = "none";
+    document.querySelector("#user-id").value = "";
 }
