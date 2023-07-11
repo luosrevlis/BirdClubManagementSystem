723f630f01f3ed27a31367ed8604820c8a853667
luosrevlis <tuanminhphamtdn@gmail.com>
2023-07-11 13:22:51 +0700

    done event reg management

--- BirdClubManagementSystem/Controllers/TournamentRegistrationsController.cs
+++ BirdClubManagementSystem/Controllers/TournamentRegistrationsController.cs
@@ -1,8 +1,8 @@
 ﻿using BirdClubManagementSystem.Data;
 using BirdClubManagementSystem.Filters;
 using BirdClubManagementSystem.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BirdClubManagementSystem.Controllers
 {
@@ -20,56 +20,31 @@ namespace BirdClubManagementSystem.Controllers
         public ActionResult Index(int tournamentId)
         {
             List<TournamentRegistration> registrations = _dbContext.TournamentRegistrations
-                .Where(tr => tr.TournamentId == tournamentId).ToList();
-            foreach (TournamentRegistration tr in registrations)
-            {
-                tr.Bird = _dbContext.Birds.Find(tr.BirdId)!;
-                tr.Bird.User = _dbContext.Users.Find(tr.Bird.UserId)!;
-                tr.Tournament = _dbContext.Tournaments.Find(tr.TournamentId)!;
-            }
+                .Where(tr => tr.TournamentId == tournamentId)
+                .Include(tr => tr.Bird)
+                .ThenInclude(bird => bird.User)
+                .Include(tr => tr.Tournament)
+                .ToList();
             return View(registrations);
         }
 
-        // GET: TournamentRegistrationsController/Details/5
-        public ActionResult Details(int id)
-        {
-            TournamentRegistration? registration = _dbContext.TournamentRegistrations.Find(id);
-            if (registration == null)
-            {
-                return NotFound();
-            }
-            registration.Bird = _dbContext.Birds.Find(registration.BirdId)!;
-            registration.Bird.User = _dbContext.Users.Find(registration.Bird.UserId)!;
-            registration.Tournament = _dbContext.Tournaments.Find(registration.TournamentId)!;
-            return View(registration);
-        }
-
-        // GET: TournamentRegistrationsController/Delete/5
+        // POST: TournamentRegistrationsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Delete(int id)
         {
             TournamentRegistration? registration = _dbContext.TournamentRegistrations.Find(id);
             if (registration == null)
             {
-                return NotFound();
-            }
-            registration.Bird = _dbContext.Birds.Find(registration.BirdId)!;
-            registration.Bird.User = _dbContext.Users.Find(registration.Bird.UserId)!;
-            registration.Tournament = _dbContext.Tournaments.Find(registration.TournamentId)!;
-            return View(registration);
-        }
-
-        // POST: TournamentRegistrationsController/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            TournamentRegistration? registration = _dbContext.TournamentRegistrations.Find(id);
-            if (registration == null)
-            {
-                return NotFound();
+                TempData.Add("notification", "Participant not found!");
+                TempData.Add("error", "");
+                return RedirectToAction("Index", "ClubEvents");
             }
             _dbContext.TournamentRegistrations.Remove(registration);
             _dbContext.SaveChanges();
+
+            TempData.Add("notification", "Participant removed!");
+            TempData.Add("success", "");
             return RedirectToAction("Index", new RouteValueDictionary(new { tournamentId = registration.TournamentId }));
         }
     }
---
+++ BirdClubManagementSystem/wwwroot/js/event-registration/index.js
@@ -0,0 +1,9 @@
+﻿function initDelete(regId) {
+    document.querySelector(".delete-container").style.display = "flex";
+    document.querySelector("#reg-id").value = regId;
+}
+
+function cancelDelete() {
+    document.querySelector(".delete-container").style.display = "none";
+    document.querySelector("#reg-id").value = "";
+}
