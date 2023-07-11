cddcf0b400b90abd53f6101c0d6fefa5efcc847e
luosrevlis <tuanminhphamtdn@gmail.com>
2023-07-11 22:46:40 +0700

    done tour standings management

--- BirdClubManagementSystem/Controllers/TournamentStandingsController.cs
+++ BirdClubManagementSystem/Controllers/TournamentStandingsController.cs
@@ -3,6 +3,7 @@ using BirdClubManagementSystem.Filters;
 using BirdClubManagementSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace BirdClubManagementSystem.Controllers
 {
@@ -20,13 +21,11 @@ namespace BirdClubManagementSystem.Controllers
         public ActionResult Index(int tournamentId)
         {
             List<TournamentStanding> standings = _dbContext.TournamentStandings
-                .Where(ts => ts.TournamentId == tournamentId).ToList();
-            foreach (TournamentStanding ts in standings)
-            {
-                ts.Bird = _dbContext.Birds.Find(ts.BirdId)!;
-                ts.Bird.User = _dbContext.Users.Find(ts.Bird.UserId)!;
-                ts.Tournament = _dbContext.Tournaments.Find(ts.TournamentId)!;
-            }
+                .Where(ts => ts.TournamentId == tournamentId)
+                .Include(ts => ts.Bird)
+                .ThenInclude(bird => bird.User)
+                .OrderBy(ts => ts.Placement)
+                .ToList();
             ViewBag.TournamentId = tournamentId;
             return View(standings);
         }
@@ -37,7 +36,9 @@ namespace BirdClubManagementSystem.Controllers
             TournamentStanding? standing = _dbContext.TournamentStandings.Find(id);
             if (standing == null)
             {
-                return NotFound();
+                TempData.Add("notification", "Ranking information not found!");
+                TempData.Add("error", "");
+                return RedirectToAction("Index", "ClubEvents");
             }
             standing.Bird = _dbContext.Birds.Find(standing.BirdId)!;
             standing.Bird.User = _dbContext.Users.Find(standing.Bird.UserId)!;
@@ -68,6 +69,9 @@ namespace BirdClubManagementSystem.Controllers
             standing.Bird = _dbContext.Birds.Find(standing.BirdId)!;
             _dbContext.TournamentStandings.Add(standing);
             _dbContext.SaveChanges();
+
+            TempData.Add("notification", "Ranking information added!");
+            TempData.Add("success", "");
             return RedirectToAction("Index", new RouteValueDictionary(new { tournamentId = standing.TournamentId }));
         }
 
@@ -77,7 +81,9 @@ namespace BirdClubManagementSystem.Controllers
             TournamentStanding? standing = _dbContext.TournamentStandings.Find(id);
             if (standing == null)
             {
-                return NotFound();
+                TempData.Add("notification", "Ranking information not found!");
+                TempData.Add("error", "");
+                return RedirectToAction("Index", "ClubEvents");
             }
             int tournamentId = standing.TournamentId;
             HashSet<int> rankedBirds = _dbContext.TournamentStandings
@@ -102,35 +108,29 @@ namespace BirdClubManagementSystem.Controllers
             standing.Bird = _dbContext.Birds.Find(standing.BirdId)!;
             _dbContext.TournamentStandings.Update(standing);
             _dbContext.SaveChanges();
+
+            TempData.Add("notification", "Ranking information updated!");
+            TempData.Add("success", "");
             return RedirectToAction("Index", new RouteValueDictionary(new { tournamentId = standing.TournamentId }));
         }
 
-        // GET: TournamentStandingsController/Delete/5
+        // POST: TournamentStandingsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Delete(int id)
         {
             TournamentStanding? standing = _dbContext.TournamentStandings.Find(id);
             if (standing == null)
             {
-                return NotFound();
-            }
-            standing.Bird = _dbContext.Birds.Find(standing.BirdId)!;
-            standing.Bird.User = _dbContext.Users.Find(standing.Bird.UserId)!;
-            standing.Tournament = _dbContext.Tournaments.Find(standing.TournamentId)!;
-            return View(standing);
-        }
-
-        // POST: TournamentStandingsController/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            TournamentStanding? standing = _dbContext.TournamentStandings.Find(id);
-            if (standing == null)
-            {
-                return NotFound();
+                TempData.Add("notification", "Ranking information not found!");
+                TempData.Add("error", "");
+                return RedirectToAction("Index", "ClubEvents");
             }
             _dbContext.Remove(standing);
             _dbContext.SaveChanges();
+
+            TempData.Add("notification", "Ranking information deleted!");
+            TempData.Add("success", "");
             return RedirectToAction("Index", new RouteValueDictionary(new { tournamentId = standing.TournamentId }));
         }
     }
---
+++ BirdClubManagementSystem/wwwroot/js/tournament-standing/index.js
@@ -0,0 +1,9 @@
+﻿function initDelete(tsId) {
+    document.querySelector(".delete-container").style.display = "flex";
+    document.querySelector("#ts-id").value = tsId;
+}
+
+function cancelDelete() {
+    document.querySelector(".delete-container").style.display = "none";
+    document.querySelector("#ts-id").value = "";
+}
