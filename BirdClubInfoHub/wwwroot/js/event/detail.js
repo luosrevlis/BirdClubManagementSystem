55886fe6f70647003e4a051681b697ebd2ba8799
luosrevlis <tuanminhphamtdn@gmail.com>
2023-07-14 21:07:31 +0700

    event rankings highlights info hub

--- BirdClubInfoHub/wwwroot/js/event/detail.js
+++ BirdClubInfoHub/wwwroot/js/event/detail.js
@@ -5,3 +5,25 @@
 function cancelRegister() {
     document.querySelector(".register-event-container").style.display = "none";
 }
+
+let btnList = document.querySelectorAll(".navtab-btn");
+let tabContent = document.querySelectorAll(".tab-content-item")
+btnList.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        // duyệt lại danh sách các btn và xóa actived cho từng btn
+        btnList.forEach((_btn) => {
+            _btn.classList.remove("active");
+        });
+        // thêm actived cho thằng vừa nhận
+        event.target.classList.add("active");
+        // duyệt danh sách các tabContent và xóa actived
+        tabContent.forEach((content) => {
+            content.classList.remove("active")
+        });
+        // thêm actived cho tab-content có data-id trùng với id của btn vừa nhận
+        let id = event.target.id; //lấy ra id của nút vừa nhấn
+        document
+            .querySelector(`.tab-content-item[data-id='${id}']`)
+            .classList.add("active")
+    });
+});
