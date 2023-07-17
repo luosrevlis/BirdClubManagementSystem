89023710c5d9b4acea814feba432b1bcce76e0ba
luosrevlis <tuanminhphamtdn@gmail.com>
2023-07-17 17:03:26 +0700

    frontend notif

---
+++ BirdClubManagementSystem/wwwroot/js/notification/index.js
@@ -0,0 +1,14 @@
+﻿const form = document.querySelector(".form"),
+    contentsField = form.querySelector(".contents-field"),
+    contentsInput = form.querySelector(".contents");
+
+function checkContents() {
+    if (contentsInput.value === "") {
+        contentsField.classList.add("invalid");
+        return false;
+    }
+    contentsField.classList.remove("invalid");
+    return true;
+}
+
+contentsInput.addEventListener("keyup", checkContents);
