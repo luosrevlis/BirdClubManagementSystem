568b32ca5e9d39e83ebb09c6734bc6fc587e1767
luosrevlis <tuanminhphamtdn@gmail.com>
2023-07-28 21:52:03 +0700

    add user dto, remove pics

---
+++ BirdClubManagementSystem/Models/DTOs/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace BirdClubManagementSystem.Models.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+
+        public string Phone { get; set; } = null!;
+
+        public string Role { get; set; } = null!;
+    }
+}
