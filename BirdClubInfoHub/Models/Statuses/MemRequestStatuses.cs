66f5ab16eda47a6354af11d5db78215baf22d400
vincentdao <vincentdaomain@gmail.com>
2023-07-28 16:02:10 +0700

    add status

---
+++ BirdClubInfoHub/Models/Statuses/MemRequestStatuses.cs
@@ -0,0 +1,22 @@
+﻿namespace BirdClubInfoHub.Models.Statuses
+{
+    public class MemRequestStatuses
+    {
+        public const string Pending = "PEN";
+        public const string Accepted = "ACC";
+        public const string Rejected = "REJ";
+        public const string PaymentReceived = "REC";
+
+        public static string Convert(String code)
+        {
+            return code switch
+            {
+                Pending => nameof(Pending),
+                Accepted => nameof(Accepted),
+                Rejected => nameof(Rejected),
+                PaymentReceived => "Payment Received",
+                _ => throw new ArgumentException(code),
+            };
+        }
+    }
+}
